8c11327fa0c9df87f2ffcee90afc9965641bd27d
tongstar <tongmon@hanmail.net>
2022-04-21 22:00:22 +0900

    22/04/21 folder update

--- Assets/Heros/test-hero/test-hero.cs
+++ Assets/Heros/test-hero/test-hero.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hero : parent
+public class test-hero : MonoBehaviour
 {
-    parent m_parent;
-
     // Start is called before the first frame update
     void Start()
     {
-        m_parent.m_speed = 1;
+        
     }
 
     // Update is called once per frame
