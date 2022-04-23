7c86b43b222d57346197b07dedf7b7caac021b9d
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-04-23 22:45:41 +0900

    22/04/23 adding cardScripts
    so hard

---
+++ Assets/Scripts/Cards/Cards.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Cards : ScriptableObject
+{
+    //구조 - 맵 클리어시 현재 가진 스킬중 카드슬롯만큼 가져와서 랜덤으로 증가이벤트 만들기.
+    //퀄1 - 100퍼 퀄2 - 50퍼 퀄3- 25퍼로 가정.
+    public int m_card_slots = 3; //첨엔 3 영웅추가로 4명까지 가능
+
+    public void SelectionApply() //카드선택적용
+    {
+        for (int i = 0; i < m_card_slots; i++)
+        {
+            if (Random.Range(1, 12) % 4 == 0) ; //m_abilities[Random.Range(1,m_card_slots)].속성부여(bool) = true; -퀄3
+            else if (Random.Range(1, 2) % 2 == 0) ; //m_abilities[Random.Range(1,m_card_slots)].m_cur_cooldown_time -=0.5f; -퀄2
+            else ; //m_abilities[Random.Range(1,m_card_slots)].데미지계수 +=0.5f; -퀄1
+        }
+    }
+}
---
+++ Assets/Scripts/Cards/CardsHolder.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsHolder //AbliltyHolder에있는 애들 가져와야함
+{
+    //m_abilities 리스트에 첫번째의 쿨다운, 계수 가져옴.
+    //hero도 가져옴. 스탯 증가도 해야함.
+    
+}
