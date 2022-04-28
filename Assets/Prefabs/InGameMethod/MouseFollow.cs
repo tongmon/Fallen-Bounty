d0e2462127e80f79ce09caab4391407eb5586c4b
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-04-28 23:12:53 +0900

    22/04/28 MouseFollow edit
    추가 수정해야함. 상태변화 3가지

--- Assets/Prefabs/InGameMethod/MouseFollow.cs
+++ Assets/Prefabs/InGameMethod/MouseFollow.cs
@@ -1,15 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.UI;
 
 public class MouseFollow : MonoBehaviour
-{   //캐릭티 겹칠시 선택오류 해결 필요 -- 아이디어 마우스오버시 둘의 초상화로 선택가능
-    [SerializeField] Image m_line;
-    public bool m_path_select;
-    public Vector2 m_vec;
-    public GameObject m_focus_object;
-    public GameObject m_focus_enemy;
-    public RaycastHit m_hit;
-    //선긋기 , 색 필요
+{   
+    private bool m_path_select;
+    private Vector2 m_vec;
+    private GameObject m_focus_object;
+    private GameObject m_focus_enemy;
+    private RaycastHit m_hit;
+
+    //????? , ?? ???
     LineRenderer m_lr;
     private void Start()
     {
@@ -21,49 +24,45 @@ public class MouseFollow : MonoBehaviour
     }
     void Update()
     {
-        if (m_path_select) //선 긋기 
+        if (Input.GetMouseButtonDown(0)) 
         {
-            m_lr.SetPosition(0, m_focus_object.transform.position);
-            if (m_hit.collider != null)
-            {
-                m_lr.SetPosition(1, m_focus_enemy.transform.position);
-            }
-            else
-            {
-                ; m_lr.SetPosition(1, m_vec);
-            }
-        }
-
-        if (Input.GetMouseButtonDown(0)) //마우스 왼쪽 버튼
-        {
-            if(m_focus_object != null) m_focus_object.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
-            Ray m_ray = Camera.main.ScreenPointToRay(Input.mousePosition); //클릭한 마우스위치의 Ray저장
+            if (m_focus_object != null) m_focus_object.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+            Ray m_ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
             Physics.Raycast(m_ray, out m_hit);
-            if (m_hit.collider != null && m_hit.transform.tag != "Enemy")//적이 아닌거만 선택
+            if (m_hit.collider != null && m_hit.transform.tag != "Enemy")
             {
                 m_focus_object = m_hit.collider.gameObject;
-                m_hit.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255); //이동하면 색이 사라짐 
+                m_hit.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
             }
         }
-        else if (Input.GetMouseButtonDown(1)) //마우스 오른쪽 버튼
+        else if (Input.GetMouseButtonDown(1)) 
         {
-            if(m_focus_enemy != null) m_focus_enemy.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+            if (m_focus_enemy != null) m_focus_enemy.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
             Ray m_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Physics.Raycast(m_ray, out m_hit);
             m_path_select = true;
-            if (m_hit.collider != null && m_hit.collider.tag == "Enemy") //적 선택시
+            if (m_hit.collider != null && m_hit.collider.tag == "Enemy") 
             {
                 m_focus_enemy = m_hit.collider.gameObject;
                 m_hit.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
             }
             else m_vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
-        if (m_path_select) //죽거나 적이사라지면 경로 삭제 해야함
+        if (m_path_select)
         {
-            if(m_hit.collider != null) m_vec = m_hit.transform.position;
+            if (m_hit.collider != null) m_vec = m_hit.transform.position;
             if (m_vec.x > 0) m_focus_object.transform.rotation = Quaternion.Euler(0, 0, 0);
             else m_focus_object.transform.rotation = Quaternion.Euler(0, 180, 0);
             m_focus_object.transform.position = Vector2.Lerp(m_focus_object.transform.position, m_vec, Time.deltaTime * 0.5f);
+            m_lr.SetPosition(0, m_focus_object.transform.position);
+            if (m_hit.collider != null && m_focus_enemy != null)
+            {
+                m_lr.SetPosition(1, m_focus_enemy.transform.position);
+            }
+            else
+            {
+                m_lr.SetPosition(1, m_vec);
+            }
         }
     }
 }
