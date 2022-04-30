39f8f264a406c8da9f73ae32f673294ca7af0dc7
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-04-30 18:12:32 +0900

    22/04/30 MouseFollow Edit
    겹칠시 이동 구현, 이동시 흔들림 현상 보완.
    목표 : y값이 큰 차이를 보이면 이동

--- Assets/Prefabs/InGameMethod/MouseFollow.cs
+++ Assets/Prefabs/InGameMethod/MouseFollow.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class MouseFollow : MonoBehaviour
 {
     private bool m_path_select;
+    private bool m_path_arrived;
     private Vector2 m_vec;
     private GameObject m_focus_object;
     private GameObject m_focus_enemy;
     private RaycastHit2D m_hit1, m_hit2;
-    private float m_horizontal_speed = 0.005f;
-    private float m_vertical_speed = 0.003f;
-    private float m_attack_range = 1f;
+    private float m_horizontal_speed = 2f;
+    private float m_vertical_speed = 1.5f;
+    private float m_attack_range = 2f;
 
     LineRenderer m_lr;
     private void Start()
@@ -36,11 +37,12 @@ public class MouseFollow : MonoBehaviour
                 gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
             }
         }
-        else if (Input.GetMouseButtonDown(1) && m_hit1.collider.gameObject == gameObject) //마우스 우 클릭
+        else if (Input.GetMouseButtonDown(1) && m_focus_object != null && m_hit1.collider.gameObject == gameObject) //마우스 우 클릭
         {
             if (m_focus_enemy != null) m_focus_enemy.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
             Vector2 m_pos2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             m_hit2 = Physics2D.Raycast(m_pos2, Vector2.zero, 0f);
+            m_path_arrived = false;
             if (m_focus_object == gameObject) m_path_select = true;  //포커스된 애가 내가 맞으면
             if (m_hit2.collider != null)
             {
@@ -57,8 +59,20 @@ public class MouseFollow : MonoBehaviour
         {
             if (m_hit2.collider != null) m_vec = m_hit2.transform.position;
             if (m_vec.x < 0) m_focus_object.transform.rotation = Quaternion.Euler(0, 180, 0); //추후 다시 수정
-            else m_focus_object.transform.rotation = Quaternion.Euler(0, 0, 0);
-
+            else m_focus_object.transform.rotation = Quaternion.Euler(0, 0, 0); //각도 계산시 y값 차이가 너무 크면안됨.
+            Vector2 dir = new Vector2(m_vec.x - m_focus_object.transform.position.x, m_vec.y - m_focus_object.transform.position.y);
+            dir.Normalize();
+            if ((m_hit2.collider != null))
+            {
+                if (Vector2.Distance(m_focus_object.transform.position, m_vec) > m_attack_range)
+                {
+                    m_focus_object.transform.Translate(new Vector2(dir.x * m_horizontal_speed * Time.deltaTime, dir.y * m_vertical_speed * Time.deltaTime), Space.World);
+                }
+                else m_path_arrived = true; //도착함
+            }
+            //if (m_path_arrived && m_focus_object.transform.position.y - m_vec.y > 0.5f) m_focus_object.transform.Translate(new Vector2(0, dir.y * m_vertical_speed * Time.deltaTime));
+            //else if (m_path_arrived && m_vec.y - m_focus_object.transform.position.y > 0.5f) m_focus_object.transform.Translate(new Vector2(0, -dir.y * m_vertical_speed * Time.deltaTime));
+            else m_focus_object.transform.Translate(new Vector2(dir.x * m_horizontal_speed * Time.deltaTime, dir.y * m_vertical_speed * Time.deltaTime), Space.World);
             m_lr.SetPosition(0, m_focus_object.transform.GetChild(0).transform.position);
             if (m_hit2.collider != null && m_focus_enemy != null)
             {
@@ -69,6 +83,22 @@ public class MouseFollow : MonoBehaviour
                 m_lr.SetPosition(1, m_vec);
             }
         }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Character" && m_path_arrived)
+        {
+            if (m_focus_object.transform.position.y > collision.gameObject.transform.position.y) Invoke("PathMoveUp", 0.2f);
+            else if (m_focus_object.transform.position.y <= collision.gameObject.transform.position.y) Invoke("PathMoveDown", 0.2f);
+        }
+    }
+    void PathMoveUp()
+    {
+        m_focus_object.transform.Translate(new Vector2(0, m_vertical_speed * Time.deltaTime * 4f), Space.World);
+    }
+    void PathMoveDown()
+    {
+        m_focus_object.transform.Translate(new Vector2(0, -m_vertical_speed) * Time.deltaTime * 4f, Space.World);
     }
 }
 //바라보는 방향 각도계산
