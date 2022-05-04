8a05ddb6236383e3c18780b0c8fd5809e7590bf3
tongstar <tongmon@hanmail.net>
2022-05-04 21:53:49 +0900

    22/05/04 move logic changed
    이동 로직 최적화

--- Assets/Prefabs/InGameMethod/MouseFollow.cs
+++ Assets/Prefabs/InGameMethod/MouseFollow.cs
@@ -10,12 +10,13 @@ public class MouseFollow : MonoBehaviour
     // 도착함
     private bool m_path_arrived;
 
-    //벡터 이동 종료 확인
-    private bool m_vec_move_done;
-
     private Vector2 m_between_vec;
-    // 방향 벡터
+
+    // 자기 자신에서 타겟 까지의 벡터
     private Vector2 m_obj_to_target_vec;
+
+    // 목표 지점 좌표
+    private Vector2 m_target_point;
 
     // 자기 자신을 선택했는지 검사하기 위한 자신의 객체
     private GameObject m_focus_object;
@@ -33,7 +34,7 @@ public class MouseFollow : MonoBehaviour
     private float m_horizontal_speed;
 
     // 수직 속도 (상하)
-    private float m_vertical_speed ;
+    private float m_vertical_speed;
 
     // 공격 사거리
     private float m_attack_range;
@@ -55,8 +56,6 @@ public class MouseFollow : MonoBehaviour
         m_attack_range = 2f;
         m_vertical_speed = 1.5f;
         m_horizontal_speed = 2f;
-
-        m_vec_move_done = false;
 
         // 줄 초기 설정
         m_lr = GetComponent<LineRenderer>();
@@ -91,7 +90,6 @@ public class MouseFollow : MonoBehaviour
             && m_hit_left_mouse.collider.gameObject == gameObject) //마우스 우 클릭
         {
             m_path_arrived = false;
-            m_vec_move_done = false;
 
             // 우측 클릭 좌표 획득
             Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -110,7 +108,7 @@ public class MouseFollow : MonoBehaviour
             // 땅 선택시
             else 
             {
-                m_obj_to_target_vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                m_target_point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 m_focus_enemy = null;
             }
         }
@@ -119,64 +117,63 @@ public class MouseFollow : MonoBehaviour
         if (m_path_select)
         {
             if (m_hit_right_mouse.collider != null)
-                m_obj_to_target_vec = m_hit_right_mouse.transform.position;
+                m_target_point = m_hit_right_mouse.transform.position;
 
             // 각도에 따른 좌우 구분을 하기 위해 각도 검사 재검토 필요
-            if (m_obj_to_target_vec.x < 0)
+            if (m_target_point.x < 0)
                 m_focus_object.transform.rotation = Quaternion.Euler(0, 180, 0);
             else 
                 m_focus_object.transform.rotation = Quaternion.Euler(0, 0, 0);
 
             // 가는 방향 설정
-            Vector2 dir = new Vector2(0, 0);
+            m_obj_to_target_vec = new Vector2(0, 0);
 
-            float distance_to_target = Vector2.Distance(m_focus_object.transform.position, m_obj_to_target_vec);
+            float distance_to_target = Vector2.Distance(m_focus_object.transform.position, m_target_point);
 
             // 길이 정해졌는데 적이 선택된 경우
             if (m_hit_right_mouse.collider != null
                 && m_hit_right_mouse.collider.gameObject.transform.tag != "Character")
             {
+                // 적의 위치가 사거리와 맞지 않음
                 if (Mathf.Abs(distance_to_target - m_attack_range) > 0.05f)
                 {
                     // 적이 사거리 안에 없는 경우
                     if (distance_to_target >= m_attack_range)
-                        dir = (Vector2)(m_hit_right_mouse.transform.position - m_focus_object.transform.position);
+                        m_obj_to_target_vec = (Vector2)(m_hit_right_mouse.transform.position - m_focus_object.transform.position);
                     // 적이 사거리보다 가까운 경우
                     else if (distance_to_target < m_attack_range)
-                        dir = (Vector2)(m_focus_object.transform.position - m_hit_right_mouse.transform.position);
+                        m_obj_to_target_vec = (Vector2)(m_focus_object.transform.position - m_hit_right_mouse.transform.position);
                 }
-                else
+                // 적의 위치가 사거리와 맞지만 각도 조정이 필요
+                else if (!m_path_arrived)
                 {
-                    m_path_arrived = true;
-                    if (!m_vec_move_done)
+                    Vector2 enemy_to_obj_vec = m_focus_object.transform.position - m_focus_enemy.transform.position;
+
+                    float angle = Mathf.Atan2(enemy_to_obj_vec.y, enemy_to_obj_vec.x) * Mathf.Rad2Deg;
+
+                    if (m_focus_enemy.transform.position.y < m_focus_object.transform.position.y)
+                        angle -= 90;
+                    else
+                        angle += 90;
+
+                    if (m_restrict_angle <= Mathf.Abs(angle))
+                        m_path_arrived = true;
+
+                    if (m_obj_to_target_vec == new Vector2(0, 0))
                     {
-                        Vector2 enemy_to_obj_vec = m_focus_object.transform.position - m_focus_enemy.transform.position;
-
-                        float angle = Mathf.Atan2(enemy_to_obj_vec.y, enemy_to_obj_vec.x) * Mathf.Rad2Deg;
-
-                        if (m_focus_enemy.transform.position.y < m_focus_object.transform.position.y)
-                            angle -= 90;
-                        else
-                            angle += 90;
-
-                        if (m_restrict_angle <= Mathf.Abs(angle))
-                            m_vec_move_done = true;
-
                         Vector2 move_vec = Quaternion.Euler(0, 0, angle) * enemy_to_obj_vec;
                         move_vec -= enemy_to_obj_vec;
                         move_vec.Normalize();
-                        dir = move_vec;
-
-                        // m_focus_object.transform.Translate(new Vector2(m_target_vec.x * m_horizontal_speed * Time.deltaTime, m_target_vec.y * m_vertical_speed * Time.deltaTime), Space.World);
+                        m_obj_to_target_vec = move_vec;
                     }
                 }
             }
             // 길이 정해졌는데 땅이 선택된 경우
             else if(distance_to_target > 0.05f)
-                dir = m_obj_to_target_vec - (Vector2)m_focus_object.transform.position;
+                m_obj_to_target_vec = m_target_point - (Vector2)m_focus_object.transform.position;
 
-            dir.Normalize();
-            m_focus_object.transform.Translate(new Vector2(dir.x * m_horizontal_speed * Time.deltaTime, dir.y * m_vertical_speed * Time.deltaTime), Space.World);
+            m_obj_to_target_vec.Normalize();
+            m_focus_object.transform.Translate(new Vector2(m_obj_to_target_vec.x * m_horizontal_speed * Time.deltaTime, m_obj_to_target_vec.y * m_vertical_speed * Time.deltaTime), Space.World);
 
             // 라인 삽입
             m_lr.SetPosition(0, m_focus_object.transform.GetChild(0).transform.position);
@@ -186,26 +183,35 @@ public class MouseFollow : MonoBehaviour
             }
             else
             {
-                m_lr.SetPosition(1, m_obj_to_target_vec);
+                m_lr.SetPosition(1, m_target_point);
             }
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.transform.tag == "Character" && !m_path_arrived && m_focus_enemy != null)//뒤에 오는애가 알아서 피하기.
+        if (collision.transform.tag != "Character" || m_focus_enemy == null || m_path_arrived)
+            return;
+
+        Vector2 between_move_vec = m_focus_object.transform.position - m_focus_enemy.transform.position;
+        if (m_focus_object.transform.position.y > collision.gameObject.transform.position.y)
         {
-            Vector2 between_move_vec = m_focus_object.transform.position - m_focus_enemy.transform.position;
-            if (m_focus_object.transform.position.y > collision.gameObject.transform.position.y)
-            {
-                m_between_vec = Quaternion.Euler(0, 0, m_object_between_angle *0.5f) * between_move_vec;
-            }
-            else 
-            {
-                m_between_vec = Quaternion.Euler(0, 0, -m_object_between_angle * 0.5f) * between_move_vec;
-            }
-            m_between_vec -= between_move_vec;
-            m_between_vec.Normalize();
-            m_focus_object.transform.Translate(new Vector2(m_between_vec.x * m_horizontal_speed * Time.deltaTime, m_between_vec.y * m_vertical_speed * Time.deltaTime ), Space.World);
+            m_between_vec = Quaternion.Euler(0, 0, m_object_between_angle * 0.5f) * between_move_vec;
         }
+        else
+        {
+            m_between_vec = Quaternion.Euler(0, 0, -m_object_between_angle * 0.5f) * between_move_vec;
+        }
+        m_between_vec -= between_move_vec;
+        m_between_vec.Normalize();
+        m_focus_object.transform.Translate(new Vector2(m_between_vec.x * m_horizontal_speed * Time.deltaTime, m_between_vec.y * m_vertical_speed * Time.deltaTime), Space.World);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag != "Character" || m_focus_enemy == null || m_path_arrived)
+            return;
+
+        // 뒤에 오는애가 알아서 피하기.
     }
 }
