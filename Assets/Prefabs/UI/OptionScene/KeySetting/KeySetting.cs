001327736b2573680e04c38c5b03f0522e6de459
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-05-11 22:38:39 +0900

    22/05/11 Lighting add
    키코드 설정해야함

---
+++ Assets/Prefabs/UI/OptionScene/KeySetting/KeySetting.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeySetting : MonoBehaviour
+{
+    public KeyCode m_key1 = KeyCode.Q;
+    public KeyCode m_key2 = KeyCode.W;
+    public KeyCode m_key3 = KeyCode.E;
+    public KeyCode m_key4 = KeyCode.R;
+    public InputField m_key_input1;
+    public InputField m_key_input2;
+    public InputField m_key_input3;
+    public InputField m_key_input4;
+    public void KeyChange1()
+    {
+        
+    }
+    public void KeyChange2()
+    {
+
+    }
+    public void KeyChange3()
+    {
+
+    }
+    public void KeyChange4()
+    {
+
+
+    }
+}
