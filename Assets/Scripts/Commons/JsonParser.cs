dc530c172fc2784dffbaef66d7f5214a9744d8aa
tongstar <68733249+tongmon@users.noreply.github.com>
2022-05-31 09:48:11 +0900

    22/05/31 Json Sub Type Added
    Nuget 패키지 관리자랑 Json 상속 클래스 받게 해주는 JsonSubType 추가함

--- Assets/Scripts/Commons/JsonParser.cs
+++ Assets/Scripts/Commons/JsonParser.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using JsonSubTypes;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
