3f8393e3b22e1406d6357c8e957bab317431db21
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-06-03 11:16:03 +0900

    22/06/03 MapJson Edit
    줄긋기만 적용하면됨

--- Assets/Scripts/Map/MapJson.cs
+++ Assets/Scripts/Map/MapJson.cs
@@ -20,17 +20,19 @@ namespace Map
     [System.Serializable]
     public class MapInfo 
     {
+        public List<Vector3> map_position = new List<Vector3>();//맵 위치 저장 리스트
+
         public List<GameObject> map_path = new List<GameObject>(); //맵 경로 저장 리스트
         public List<GameObject> map_path1 = new List<GameObject>(); 
         public List<GameObject> map_path2 = new List<GameObject>(); 
 
-        public List <Sprite> sprite = new List<Sprite>(); //스트라이트 리스트
-        public List<Sprite> sprite1 = new List<Sprite>();
-        public List<Sprite> sprite2 = new List<Sprite>();
-
         public List <eMapType> mapType = new List <eMapType>(); //맵타입 적용 리스트
         public List<eMapType> mapType1 = new List<eMapType>();
         public List<eMapType> mapType2 = new List<eMapType>();
+
+        public List<Sprite> sprite = new List<Sprite>(); //스트라이트 리스트
+        public List<Sprite> sprite1 = new List<Sprite>();
+        public List<Sprite> sprite2 = new List<Sprite>();
 
         public bool elite_exist = false; //엘리트 확인용 부울변수
         public bool elite_exist1 = false;
@@ -40,29 +42,36 @@ namespace Map
     {
         [SerializeField] Sprite[] m_sprite;
         [SerializeField] GameObject m_prefab_canvas;
-        [SerializeField] GameObject m_saved_map;
         LineRenderer l;//이걸써보자
         private static string m_save_path => "Assets/Resources/MapJson/";
 
         private void Start()
         {
-            Instantiate(l, m_prefab_canvas.transform);//라인을 따로 생성가능 이걸 이용해야함
             if (!Directory.Exists(m_save_path)) //디렉토리가 없으면
             {
                 Directory.CreateDirectory(m_save_path); //만듦
                 MapInfo m_mapInfo = new MapInfo();//맵 인포 객체 생성
-
-                for(int i =0; i < 30; i++) //저장맵 초기화
-                {
-                    m_saved_map.transform.GetChild(i).transform.position = Resources.Load<GameObject>("MapPrefab").transform.GetChild(i).transform.position;
-                }
-                Instantiate(m_saved_map, m_prefab_canvas.transform); //인스턴스 화
-                GameObject map = GameObject.FindGameObjectWithTag("Map");
+                Instantiate(Resources.Load<GameObject>("MapPrefab"), m_prefab_canvas.transform); //인스턴스 화
+                GameObject map = GameObject.FindGameObjectWithTag("Map");//객체 연결
                 for (int i = 0; i < 30; i += 3) //맵 경로설정
                 {
-                    map.transform.GetChild(i).transform.Translate(new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f))); //초기화와 동시에 위치 랜덤화
-                    map.transform.GetChild(i+1).transform.Translate(new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f))); 
-                    map.transform.GetChild(i+2).transform.Translate(new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f)));
+                    map.transform.GetChild(i).transform.Translate(new Vector2(Random.Range(-0.3f, 0.3f), Random.Range(-0.3f, 0.3f))); //위치 랜덤화
+                    map.transform.GetChild(i+1).transform.Translate(new Vector2(Random.Range(-0.3f, 0.3f), Random.Range(-0.3f, 0.3f))); 
+                    map.transform.GetChild(i+2).transform.Translate(new Vector2(Random.Range(-0.3f, 0.3f), Random.Range(-0.3f, 0.3f)));
+                    m_mapInfo.map_position.Add(map.transform.GetChild(i).transform.position);//랜덤화된 위치 저장
+                    m_mapInfo.map_position.Add(map.transform.GetChild(i+1).transform.position);
+                    m_mapInfo.map_position.Add(map.transform.GetChild(i+2).transform.position);
+
+                    map.transform.GetChild(i).GetComponent<LineRenderer>().startWidth = 0.05f;//길이 설정 먼저하기
+                    map.transform.GetChild(i).GetComponent<LineRenderer>().endWidth = 0.05f;
+                    map.transform.GetChild(i).GetComponent<LineRenderer>().SetPosition(0, map.transform.GetChild(i).transform.position);
+                    map.transform.GetChild(i+1).GetComponent<LineRenderer>().startWidth = 0.05f;
+                    map.transform.GetChild(i+1).GetComponent<LineRenderer>().endWidth = 0.05f;
+                    map.transform.GetChild(i+1).GetComponent<LineRenderer>().SetPosition(0, map.transform.GetChild(i+1).transform.position);
+                    map.transform.GetChild(i+2).GetComponent<LineRenderer>().startWidth = 0.05f;
+                    map.transform.GetChild(i+2).GetComponent<LineRenderer>().endWidth = 0.05f;
+                    map.transform.GetChild(i+2).GetComponent<LineRenderer>().SetPosition(0, map.transform.GetChild(i+2).transform.position);
+
                     m_mapInfo.map_path.Add(map.transform.GetChild(Random.Range(i, i + 2)).gameObject); //얘네 수정 해야됨.
                     m_mapInfo.map_path1.Add(map.transform.GetChild(Random.Range(i, i + 2)).gameObject);
                     m_mapInfo.map_path2.Add(map.transform.GetChild(Random.Range(i, i + 2)).gameObject);
@@ -73,6 +82,7 @@ namespace Map
 
                 for (int i = 0; i < m_mapInfo.map_path.Count-1; i++) //랜덤 맵타입 삽입
                 {
+                    m_mapInfo.map_path[i].GetComponent<LineRenderer>().SetPosition(1,m_mapInfo.map_path[i+1].transform.position);
                     int map_number = Random.Range(0, 6);
                     if (map_number == 1 && m_mapInfo.elite_exist == true)
                     {
@@ -87,6 +97,7 @@ namespace Map
                 }
                 for (int i = 0; i < m_mapInfo.map_path1.Count-1; i++) 
                 {
+                    m_mapInfo.map_path1[i].GetComponent<LineRenderer>().SetPosition(1, m_mapInfo.map_path1[i + 1].transform.position);
                     int map_number1 = Random.Range(0, 6);
                     if (map_number1 == 1 && m_mapInfo.elite_exist1 == true)
                     {
@@ -101,6 +112,7 @@ namespace Map
                 }
                 for (int i = 0; i < m_mapInfo.map_path2.Count-1; i++)
                 {
+                    m_mapInfo.map_path2[i].GetComponent<LineRenderer>().SetPosition(1, m_mapInfo.map_path2[i + 1].transform.position);
                     int map_number2 = Random.Range(0, 6);
                     if (map_number2 == 1 && m_mapInfo.elite_exist == true)
                     {
@@ -116,7 +128,6 @@ namespace Map
                 m_mapInfo.mapType.Add(eMapType.Boss);//맵종류 보스넣기
                 m_mapInfo.mapType1.Add(eMapType.Boss);
                 m_mapInfo.mapType2.Add(eMapType.Boss);
-
                 string save_json = JsonUtility.ToJson(m_mapInfo, true); //json으로 변환
                 string save_file_path = m_save_path + "MapJson.json"; //경로설정
                 File.WriteAllText(save_file_path, save_json); //json 생성
@@ -128,7 +139,17 @@ namespace Map
                 string save_file = File.ReadAllText(save_file_path); //json 읽기
                 MapInfo m_mapInfo = JsonUtility.FromJson<MapInfo>(save_file); //json 적용
                 Debug.Log(save_file);
-                Instantiate(m_saved_map, m_prefab_canvas.transform);//저장된 맵 인스턴스화
+
+                Instantiate(Resources.Load<GameObject>("MapPrefab"), m_prefab_canvas.transform);//저장된 맵 인스턴스화
+                GameObject map = GameObject.FindGameObjectWithTag("Map");//다시 찾아서 연결
+
+                for(int i =0;i<=30; i++)//위치 일치화, 줄 긋기가 필요함
+                {
+                    map.transform.GetChild(i).transform.position = m_mapInfo.map_position[i];
+                    map.transform.GetChild(i).GetComponent<LineRenderer>().startWidth = 0.05f;
+                    map.transform.GetChild(i).GetComponent<LineRenderer>().endWidth = 0.05f;
+                    map.transform.GetChild(i).GetComponent<LineRenderer>().SetPosition(0, map.transform.GetChild(i).transform.position);
+                }
             }
         }
     }
