90bc554bc71b0a7bd82372fabe9f7f2836474297
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-06-13 17:22:56 +0900

    22/06/13 MapJson Edit
    JArray 형태로 저장완료, 로직필요

--- Assets/Scripts/Map/MapJson.cs
+++ Assets/Scripts/Map/MapJson.cs
@@ -2,30 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class MapJson : MonoBehaviour
 {
     [SerializeField] GameObject m_PrefabCanvas;//인스턴스화 위치
     [SerializeField] GameObject m_map_prefab;
-    List <MapNode> m_node; //json으로 불러오기전
     public string m_path;//파일경로
+    List<MapNode> m_node;
     private void Start()
     {
+        m_node = new List<MapNode>();
         m_path = "Assets/Resources/MapJson/";
         Instantiate(m_map_prefab, m_PrefabCanvas.transform);
         GameObject map = GameObject.FindGameObjectWithTag("Map");
+        JArray jarray = new JArray();
         if (!File.Exists(m_path))
         {
-            m_node = new List<MapNode>();//새로운 노드 생성
             for (int i = 0; i < 40; i++)
             {
-                MapNode temp = new MapNode();
+                MapNode temp = new MapNode();//노드에 넣을 녀석들 생성
                 temp.m_num = i;
                 temp.m_mapType = eMapType.Common;
                 temp.m_object = map.transform.GetChild(i).gameObject;
                 m_node.Add(temp);
             }
-            for(int i =0; i < m_node.Count; i++)
+            for (int i =0; i < m_node.Count; i++)
             {
                 int j = 0;
                 if (i % 4 == 0)
@@ -53,13 +56,13 @@ public class MapJson : MonoBehaviour
                 {
                     m_node[j].m_parent.Add(m_node[i]);
                 }
-            }
-            JsonParser.CreateJsonFile(m_path, "MapJson", JsonUtility.ToJson(m_node));//저장된 객체가 없음
+                jarray.Add(JsonUtility.ToJson(m_node[i]));
+            }//부모 자식 관계생성
+            JsonParser.CreateJsonFile(m_path, "MapJson", jarray.ToString());//저장된 객체가 없음
         }
         else
         {
             m_node = JsonParser.LoadJsonArrayToList<MapNode>(m_path);
-            Debug.Log(m_node.ToString());
         }
     }
 }
