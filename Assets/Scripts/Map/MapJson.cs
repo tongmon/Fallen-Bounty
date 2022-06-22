f9003e74cb9441c422c3f84aa2e51067168964e7
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-06-22 15:53:02 +0900

    22/06/22 MapJson Edit
    부모생성 오류수정중

--- Assets/Scripts/Map/MapJson.cs
+++ Assets/Scripts/Map/MapJson.cs
@@ -33,10 +33,14 @@ public class MapJson : MonoBehaviour
                 temp.m_position = obj.transform.position;
                 m_node.Add(temp);
             }
-            for (int i =0; i < m_node.Count; i++) //가끔 선택안된애들이 생긴다 - 아이디어 : 한번 포문 더 돌아서 랜덤 부모에게 연결.
+            MapNode boss = new MapNode();//보스 생성
+            boss.m_num = 40;
+            boss.m_mapType = eMapType.Boss;
+            m_node.Add(boss);
+            for (int i =0; i < m_node.Count - 1; i++) 
             {
                 int node_num = 0;
-                int map_num = Random.Range(0, 6);
+                int map_num = Random.Range(0, 7);
                 if (i == 36 || i == 37 || i == 38 || i == 39)
                 {
                     node_num = 40;
@@ -45,63 +49,66 @@ public class MapJson : MonoBehaviour
                 {
                     if (i % 4 == 0)
                     {
-                        node_num = Random.Range(i + 4, i + 7);
+                        node_num = Random.Range(i + 4, i + 8);
                     }
                     else if (i % 4 == 1)
                     {
-                        node_num = Random.Range(i + 3, i + 6);
+                        node_num = Random.Range(i + 3, i + 7);
                     }
                     else if (i % 4 == 2)
                     {
-                        node_num = Random.Range(i + 2, i + 5);
-
+                        node_num = Random.Range(i + 2, i + 6);
                     }
                     else if (i % 4 == 3)
                     {
-                        node_num = Random.Range(i + 1, i + 4);
+                        node_num = Random.Range(i + 1, i + 5);
                     }
-                    m_node[node_num].m_parent_num = i;
+                    m_node[node_num].m_parent_num.Add(i);
                 }
                 m_node[i].m_child_num.Add(node_num);
                 if (i > 3)
                 {
-                    if (m_node[i].m_child_num[0] == 40 || m_node[m_node[i].m_parent_num].m_mapType == eMapType.Elite || m_node[m_node[i].m_child_num[0]].m_mapType == eMapType.Elite)
+                    bool isElite = false;
+                    for(int j = 0; j< m_node[j].m_parent_num.Count; j++)
+                    {
+                        if (m_node[m_node[j].m_parent_num[j]].m_mapType == eMapType.Elite) isElite = true;
+                    }
+                    if (m_node[i].m_child_num[0] == 40 || isElite || m_node[m_node[i].m_child_num[0]].m_mapType == eMapType.Elite)
                     {
                         while (map_num != 1)
                         {
-                            map_num = Random.Range(0, 6);
+                            map_num = Random.Range(0, 7);
                         }
                     }
                 }
                 m_node[i].m_mapType = (eMapType)map_num;
                 jarray.Add(JObject.Parse(JsonUtility.ToJson(m_node[i], true)));
             }//부모 자식 관계생성
-            for (int i = 4; i < m_node.Count; i++) { 
-                if(m_node[i].m_parent_num == 0) //부모가 없다.
+            for (int i = 4; i < m_node.Count - 1; i++) { 
+                if(m_node[i].m_parent_num.Count == 0) //검사 잘하는데 뭔가 오류가 난다.
                 {
                     int node_num = 0;
                     if (i % 4 == 0)
                     {
-                        node_num = Random.Range(i - 1, i - 4);
+                        node_num = Random.Range(i - 4, i);
                     }
                     else if (i % 4 == 1)
                     {
-                        node_num = Random.Range(i -2 , i - 5);
+                        node_num = Random.Range(i - 5 , i - 1);
                     }
                     else if (i % 4 == 2)
                     {
-                        node_num = Random.Range(i -3, i -6);
-
+                        node_num = Random.Range(i - 6, i - 2);
                     }
                     else if (i % 4 == 3)
                     {
-                        node_num = Random.Range(i -4, i -7);
+                        node_num = Random.Range(i - 7, i - 3);
                     }
-                    m_node[i].m_parent_num = node_num;
+                    m_node[i].m_parent_num.Add(node_num);
                     m_node[node_num].m_child_num.Add(i);
                 }
             } //함더 포문 돌기.
-            for (int i = 0; i < m_node.Count; i++)
+            for (int i = 0; i < m_node.Count - 1; i++)
             {
                 GameObject obj = map.transform.GetChild(i).gameObject;
                 if(m_node[i].m_child_num.Count > 1)
@@ -116,7 +123,7 @@ public class MapJson : MonoBehaviour
         else
         {
             m_node = JsonParser.LoadJsonArrayToList<MapNode>(m_path + "MapJson");
-            for (int i =0; i< m_node.Count; i++)//인스턴스와 일치화
+            for (int i =0; i< m_node.Count - 1; i++)//인스턴스와 일치화
             {
                 GameObject obj = map.transform.GetChild(i).gameObject;
                 obj.transform.position = m_node[i].m_position;
@@ -125,10 +132,14 @@ public class MapJson : MonoBehaviour
                 obj.GetComponent<LineRenderer>().SetPosition(1, obj.transform.position);
                 obj.GetComponent<LineRenderer>().SetPosition(2, obj.transform.position);
             }
-            for (int i = 0; i < m_node.Count; i++)
+            for (int i = 0; i < m_node.Count - 1; i++)
             {
                 GameObject obj = map.transform.GetChild(i).gameObject;
                 obj.GetComponent<LineRenderer>().SetPosition(0, map.transform.GetChild(m_node[i].m_child_num[0]).transform.position);
+                if (m_node[i].m_child_num.Count > 1)
+                {
+                    obj.GetComponent<LineRenderer>().SetPosition(2, map.transform.GetChild(m_node[i].m_child_num[1]).transform.position);
+                }
             }
         }
     }
--- Assets/Scripts/Map/MapNode.cs
+++ Assets/Scripts/Map/MapNode.cs
@@ -23,13 +23,13 @@ public class MapNode
     public Vector3 m_position;//위치, 선위치.
     public Sprite m_sprite;
     public eMapType m_mapType;
-    public int m_parent_num;
+    public List <int> m_parent_num;
     public List <int> m_child_num;
 
     public MapNode()
     {
         m_mapType = eMapType.Common;
-        m_parent_num = 0;
+        m_parent_num = new List<int>();
         m_child_num = new List<int>();
         this.m_position = new Vector3(0,0,0);
         this.m_sprite = null;
