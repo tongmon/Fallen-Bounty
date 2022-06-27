5c87f3f776d72816550cb8c65ac4b820f87cf461
최유섭 <80661998+9503chl@users.noreply.github.com>
2022-06-27 13:12:57 +0900

    22/06/27 LogScene Update
    로그씬 만드는중 Json 필요해보임

--- Assets/Prefabs/UI/LogScene/LogScene.cs
+++ Assets/Prefabs/UI/LogScene/LogScene.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LogScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject ItemButtonList;
+    [SerializeField] GameObject CharacterButtonList;
+    [SerializeField] GameObject StageButtonList;
+    [SerializeField] GameObject ChallangesButtonList;
+    [SerializeField] GameObject ClearButtonList;
 
-    // Update is called once per frame
-    void Update()
+    public void ItemButton()
     {
-        
+        ItemButtonList.SetActive(true);
+        ChallangesButtonList.SetActive(false);
+        StageButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(false);
+        ClearButtonList.SetActive(false);
+    }
+    public void CharacterButton()
+    {
+        ItemButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(true);
+        StageButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(false);
+        ClearButtonList.SetActive(false);
+    }
+    public void StageButton()
+    {
+        ItemButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(false);
+        StageButtonList.SetActive(true);
+        ChallangesButtonList.SetActive(false);
+        ClearButtonList.SetActive(false);
+    }
+    public void ChallengeButton()
+    {
+        ItemButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(false);
+        StageButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(true);
+        ClearButtonList.SetActive(false);
+    }
+    public void ClearButton()
+    {
+        ItemButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(false);
+        StageButtonList.SetActive(false);
+        ChallangesButtonList.SetActive(false);
+        ClearButtonList.SetActive(true);
+    }
+    public void BackButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title_Scene");
     }
 }
