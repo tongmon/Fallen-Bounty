30d4cdd2c0bb40270c4472d32d88b90f7f3a61a1
tongstar <68733249+tongmon@users.noreply.github.com>
2022-07-14 15:19:08 +0900

    22/07/14 friction update
    마찰력 완료, stage에서 값 전달 작업해야됨

--- Assets/Scripts/Heroes/Common/HeroPhysicsComponent.cs
+++ Assets/Scripts/Heroes/Common/HeroPhysicsComponent.cs
@@ -26,21 +26,21 @@ public class HeroPhysicsComponent : PhysicsComponent
         // m_rigidbody.position += ((Hero)m_data).m_vec_direction.normalized * m_move_velocity * Time.deltaTime;
 
         // 영웅이 가야 하는 방향으로 현재 속도를 주면 어느 정도 속도가 이동 속도에 가해지는 지가 나옴 (벡터 중 이동 하려는 방향으로의 크기를 추출한다고 보면 됨.)
-        Vector2 applied_movement = m_velocity.normalized * ((Hero)m_data).m_vec_direction.normalized;
+        Vector2 applied_movement = m_velocity * ((Hero)m_data).m_vec_direction.normalized;
         float x = m_velocity.x, y = m_velocity.y;
 
         if (Mathf.Abs(applied_movement.x) < Mathf.Abs(((Hero)m_data).m_vec_direction.x * m_move_velocity.x))
-            x = ((Hero)m_data).m_vec_direction.x * m_move_velocity.x;
+            x = ((Hero)m_data).m_vec_direction.normalized.x * m_move_velocity.x;
 
         if(Mathf.Abs(applied_movement.y) < Mathf.Abs(((Hero)m_data).m_vec_direction.y * m_move_velocity.y))
-            y = ((Hero)m_data).m_vec_direction.y * m_move_velocity.y;
+            y = ((Hero)m_data).m_vec_direction.normalized.y * m_move_velocity.y;
 
         m_velocity = new Vector2(x, y);
 
         //m_velocity += ((Hero)m_data).m_vec_direction.normalized * m_move_velocity;
 
         #region 마찰력 처리(후처리 해야 함)
-        Vector2 friction = new Vector2(2300, 2300); // new Vector2(m_affected_friction.x, m_affected_friction.y); // 마찰력 크기(방향이 포함되면 안됨), 이렇게 하면 안되고 외부에서 얻어와야 됨
+        Vector2 friction = new Vector2(200, 200); // new Vector2(m_affected_friction.x, m_affected_friction.y); // 마찰력 크기(방향이 포함되면 안됨), 이렇게 하면 안되고 외부에서 얻어와야 됨
         friction *= -m_velocity.normalized; // 마찰력은 가해지는 속도의 반대 방향으로 적용
         Vector2 accel = friction / m_mass;
         Vector2 friction_velocity = m_velocity + accel * Time.deltaTime;
